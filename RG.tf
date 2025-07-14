2546f7322fde791ab5b311fad6986aab9cf4b744
ShivamTyagi <Tyagishivam123.st@gmail.com>
2025-07-15 05:24:18 +0530

    new resources

--- RG.tf
+++ RG.tf
@@ -20,18 +20,11 @@ provider "azurerm" {
   features {}
   subscription_id = "ff5b2fc2-2ebb-4a22-9e25-a8941510f4c7"
 }
-resource "azurerm_resource_group" "Aprilfool1" {
-  name     = "Aprilfool_1stApril"
-  location = "centralindia"
+resource "azurerm_resource_group" "RainySeason" {
+  name     = "Baarish_Season"
+  location = "central india"
 }
-resource "azurerm_storage_account" "example" {
-  name                     = "mystorageaccount12345"  # Must be globally unique, only lowercase alphanumeric characters allowed.
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"  # Choose your desired replication type (e.g., GRS, LRS, ZRS).
-  public_network_access_enabled = false  # Enable public access if needed.
-  tags = {
-    environment = "staging"
-  }
+resource "azurerm_resource_group" "FloodySeason" {
+  name     = "Flood_Season"
+  location = "Western India"
 }
