a15593c79fe4a61c72929c9a5bbef813b2d6361b
ShivamTyagi <Tyagishivam123.st@gmail.com>
2025-08-10 18:41:12 +0530

    New update into main

--- RG.tf
+++ RG.tf
@@ -6,21 +6,30 @@ terraform {
     }
   }
 }
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "Agrawest"
-#     storage_account_name = "aug15ind"
-#     container_name       = "backend"
-#     key                  = "terraform.tfstate"
-#     subscription_id      = "5e4fafbb-ef69-4e17-8835-68030d81f758"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "Agrawest"
+    storage_account_name = "aug15ind"
+    container_name       = "backend"
+    key                  = "terraform.tfstate"
+    subscription_id      = "5e4fafbb-ef69-4e17-8835-68030d81f758"
+  }
+}
+resource "azurerm_storage_container" "Backend" {
+  name                  = "backend"
+  storage_account_id    = azurerm_storage_account.aug.id
+  container_access_type = "private" 
+}
 # new changes done
 provider "azurerm" {
   features {}
   subscription_id = "5e4fafbb-ef69-4e17-8835-68030d81f758"
 }
-# 2 RG hain
+# 3 RG hain
+resource "azurerm_resource_group" "Agra" {
+  name     = "Agrawest"
+  location = "West india"
+}
 resource "azurerm_resource_group" "RainySeason" {
   name     = "Baarish_Season"
   location = "central india"
@@ -29,5 +38,15 @@ resource "azurerm_resource_group" "FloodySeason" {
   name     = "Flood_Season"
   location = "West India"
 }
-
+# 1 Storage account
+resource "azurerm_storage_account" "aug" {
+  name                     = "aug15ind"
+  resource_group_name      = azurerm_resource_group.Agra.name
+  location                 = azurerm_resource_group.Agra.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = {
+    environment = "staging"
+  }
+}
 
