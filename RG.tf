ffb9b822da859f608d973777d62343125fe77635
Tyagishivam123 <tyagishivam123.st@gmail.com>
2025-09-02 14:59:13 +0530

    Update RG.tf

--- RG.tf
+++ RG.tf
@@ -6,18 +6,18 @@ terraform {
     }
   }
 }
-terraform {
- backend "azurerm" {
-   resource_group_name  = "Agrawest"
-    storage_account_name = "aug15"
-   container_name       = "statefile"
-    key                  = "git.tfstate"
-   subscription_id      = "5e4fafbb-ef69-4e17-8835-68030d81f758"
-   }
- }
+#terraform {
+ #backend "azurerm" {
+ #  resource_group_name  = "Agrawest"
+  #  storage_account_name = "sept15"
+   #container_name       = "statefile"
+    #key                  = "git.tfstate"
+   #subscription_id      = "5e4fafbb-ef69-4e17-8835-68030d81f758"
+   #}
+ #}
 resource "azurerm_storage_container" "stg" {
   name                  = "statefile"
- storage_account_id    = azurerm_storage_account.aug.id
+ storage_account_id    = azurerm_storage_account.sept.id
  container_access_type = "private" 
 }
 # new changes done
@@ -39,8 +39,8 @@ resource "azurerm_resource_group" "Agra" {
   #location = "West India"
 #}
 # 1 Storage account
-resource "azurerm_storage_account" "aug" {
- name                     = "aug15"
+resource "azurerm_storage_account" "sept" {
+ name                     = "sept15"
 resource_group_name      = azurerm_resource_group.Agra.name
 location                 = azurerm_resource_group.Agra.location
 account_tier             = "Standard"
@@ -49,6 +49,7 @@ account_tier             = "Standard"
     environment = "staging"
   }
 }
+
 
 
 
