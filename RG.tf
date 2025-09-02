bbb34710fce16f0f1505c19940ef94f7482298ee
Tyagishivam123 <tyagishivam123.st@gmail.com>
2025-09-02 14:20:11 +0530

    Backend RG.tf

--- RG.tf
+++ RG.tf
@@ -15,40 +15,41 @@ terraform {
    subscription_id      = "5e4fafbb-ef69-4e17-8835-68030d81f758"
    }
  }
-resource "azurerm_storage_container" "stg" {
-  name                  = "stg"
-  storage_account_id    = azurerm_storage_account.aug.id
-  container_access_type = "private" 
-}
+#resource "azurerm_storage_container" "stg" {
+#  name                  = "stg"
+#  storage_account_id    = azurerm_storage_account.aug.id
+#  container_access_type = "private" 
+#}
 # new changes done
 provider "azurerm" {
   features {}
   subscription_id = "5e4fafbb-ef69-4e17-8835-68030d81f758"
 }
 # 3 RG hain
-resource "azurerm_resource_group" "Agra" {
-  name     = "Agrawest"
-  location = "West india"
-}
-resource "azurerm_resource_group" "RainySeason" {
-  name     = "Baarish_Season"
-  location = "central india"
-}
-resource "azurerm_resource_group" "FloodySeason" {
-  name     = "Flood_Season"
-  location = "West India"
-}
+#resource "azurerm_resource_group" "Agra" {
+ # name     = "Agrawest"
+  #location = "West india"
+#}
+#resource "azurerm_resource_group" "RainySeason" {
+ # name     = "Baarish_Season"
+  #location = "central india"
+#}
+#resource "azurerm_resource_group" "FloodySeason" {
+ # name     = "Flood_Season"
+  #location = "West India"
+#}
 # 1 Storage account
-resource "azurerm_storage_account" "aug" {
-  name                     = "aug15"
-  resource_group_name      = azurerm_resource_group.Agra.name
-  location                 = azurerm_resource_group.Agra.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags = {
-    environment = "staging"
-  }
-}
+#resource "azurerm_storage_account" "aug" {
+ # name                     = "aug15"
+  #resource_group_name      = azurerm_resource_group.Agra.name
+  #location                 = azurerm_resource_group.Agra.location
+  #account_tier             = "Standard"
+  #account_replication_type = "LRS"
+  #tags = {
+   # environment = "staging"
+  #}
+#}
+
 
 
 
